08a85a6a62ab4b8230c237106acfde0adcf6acf3
ayurn <65171090+ayurn@users.noreply.github.com>
2021-05-13 00:49:28 +0530

    Added : more parameters

--- src/main/java/GenericMax.java
+++ src/main/java/GenericMax.java
@@ -4,22 +4,30 @@ public class GenericMax<E extends Comparable> {
     E firstPosition;
     E secondPosition;
     E thirdPosition;
+    E fourthPosition;
+    E fifthPosition;
 
-    public GenericMax(E firstPosition, E secondPosition, E thirdPosition) {
+    public GenericMax(E firstPosition, E secondPosition, E thirdPosition, E fourthPosition, E fifthPosition) {
         this.firstPosition = firstPosition;
         this.secondPosition = secondPosition;
         this.thirdPosition = thirdPosition;
+        this.fourthPosition = fourthPosition;
+        this.fifthPosition = fifthPosition;
     }
 
     public E maximumGeneric(){
-        return genericMaximum(firstPosition,secondPosition,thirdPosition);
+        return genericMaximum(firstPosition, secondPosition, thirdPosition, fourthPosition, fifthPosition);
     }
-    public <E extends Comparable> E genericMaximum(E firstPosition, E secondPosition, E thirdPosition){
-        E maxPosition=firstPosition;
+    public <E extends Comparable> E genericMaximum(E firstPosition, E secondPosition, E thirdPosition, E fourthPosition, E fifthPosition){
+        E maxPosition = firstPosition;
         if(secondPosition.compareTo(maxPosition) > 0)
-            maxPosition=secondPosition;
+            maxPosition = secondPosition;
         if(thirdPosition.compareTo(maxPosition) > 0)
-            maxPosition=thirdPosition;
+            maxPosition = thirdPosition;
+        if(fourthPosition.compareTo(maxPosition) > 0)
+            maxPosition = fourthPosition;
+        if(fifthPosition.compareTo(maxPosition) > 0)
+            maxPosition = fifthPosition;
         return maxPosition;
     }
 }
--- src/test/java/GenericMaxTest.java
+++ src/test/java/GenericMaxTest.java
@@ -7,7 +7,7 @@ public class GenericMaxTest {
     // Integer
     @Test
     public void givenThreeIntegers_shouldReturnMaxPosition(){
-        GenericMax genericMax = new GenericMax(9,5,2);
+        GenericMax genericMax = new GenericMax(9,5,2, 3, 4);
         Integer maxInt = (Integer) genericMax.maximumGeneric();
         Assertions.assertEquals(Integer.valueOf(9),maxInt);
     }
@@ -15,36 +15,36 @@ public class GenericMaxTest {
     //Float
     @Test
     public void givenThreeFloats_shouldReturnMaxPosition(){
-        GenericMax genericMax = new GenericMax(2.3,2.1,2.2);
+        GenericMax genericMax = new GenericMax(2.7,2.1,2.2, 2.3, 2.5);
         Double maxFloat = (Double) genericMax.maximumGeneric();
-        Assertions.assertEquals(Double.valueOf(2.3),maxFloat);
+        Assertions.assertEquals(Double.valueOf(2.7),maxFloat);
     }
 
     //String
     @Test
     public void givenThreeStrings_shouldReturnMaxPosition(){
-        GenericMax genericMax = new GenericMax("Pineapple", "Apple", "Banana");
+        GenericMax genericMax = new GenericMax("Pineapple", "Apple", "Banana", "Ice", "An");
         String maxString = (String) genericMax.maximumGeneric();
         Assertions.assertEquals(String.valueOf("Pineapple"),maxString);
     }
     //refactor2
     @Test
     public void FirstNumMaxTest() {
-        GenericMax genericMax = new GenericMax(9,5,2);
+        GenericMax genericMax = new GenericMax(9,5,2, 3, 4);
         Integer maxInt = (Integer) genericMax.maximumGeneric();
         Assertions.assertEquals(Integer.valueOf(9),maxInt);
     }
 
     @Test
     public void SecondNumMaxTest() {
-        GenericMax genericMax = new GenericMax(5,9,2);
+        GenericMax genericMax = new GenericMax(5,9,2, 3, 4);
         Integer maxInt = (Integer) genericMax.maximumGeneric();
         Assertions.assertEquals(Integer.valueOf(9),maxInt);
     }
 
     @Test
     public void ThirdNumMaxTest() {
-        GenericMax genericMax = new GenericMax(9,5,21);
+        GenericMax genericMax = new GenericMax(9,5,21, 4, 8);
         Integer maxInt = (Integer) genericMax.maximumGeneric();
         Assertions.assertEquals(Integer.valueOf(21),maxInt);
     }
@@ -52,15 +52,15 @@ public class GenericMaxTest {
     //UC-2 Given 3 Floats find the maximum //TC--2.1 //Floats
     @Test
     public void givenMaxFloatAtFirstPosition(){
-        GenericMax genericMax = new GenericMax(2.3,2.1,2.2);
+        GenericMax genericMax = new GenericMax(2.21,2.1,2.2, 2.19, 2.14);
         Double maxFloat = (Double) genericMax.maximumGeneric();
-        Assertions.assertEquals(Double.valueOf(2.3),maxFloat);
+        Assertions.assertEquals(Double.valueOf(2.21),maxFloat);
     }
 
     //TC--2.2
     @Test
     public void givenMaxFloatAtSecondPosition(){
-        GenericMax genericMax = new GenericMax(2.3,2.34,2.2);
+        GenericMax genericMax = new GenericMax(2.3,2.34,2.2, 2.22, 2.15);
         Double maxFloat = (Double) genericMax.maximumGeneric();
         Assertions.assertEquals(Double.valueOf(2.34),maxFloat);
     }
@@ -68,14 +68,14 @@ public class GenericMaxTest {
     //TC--2.3
     @Test
     public void givenMaxFloatAtThirdPosition(){
-        GenericMax genericMax = new GenericMax(2.3,2.1,2.23);
+        GenericMax genericMax = new GenericMax(2.3,2.1,2.23, 2.22, 2.16);
         Double maxFloat = (Double) genericMax.maximumGeneric();
         Assertions.assertEquals(Double.valueOf(2.23),maxFloat);
     }
     //TC--3.1
     @Test
     public void givenMaxStringAtFirstPosition_shouldReturnSame(){
-        GenericMax genericMax = new GenericMax("Pineapple", "Apple", "Banana");
+        GenericMax genericMax = new GenericMax("Pineapple", "Apple", "Banana", "Ice", "An");
         String maxString = (String) genericMax.maximumGeneric();
         Assertions.assertEquals(String.valueOf("Pineapple"),maxString);
     }
@@ -83,7 +83,7 @@ public class GenericMaxTest {
     //TC--3.2
     @Test
     public void givenMaxStringAtSecondPosition_shouldReturnSame(){
-        GenericMax genericMax = new GenericMax("Apple", "Pineapple", "Banana");
+        GenericMax genericMax = new GenericMax("Apple", "Pineapple", "Banana", "An", "Ice");
         String maxString = (String) genericMax.maximumGeneric();
         Assertions.assertEquals(String.valueOf("Pineapple"),maxString);
     }
@@ -91,7 +91,7 @@ public class GenericMaxTest {
     //TC--3.3
     @Test
     public void givenMaxStringAtThirdPosition_shouldReturnSame(){
-        GenericMax genericMax = new GenericMax("Apple", "Banana", "Pineapple");
+        GenericMax genericMax = new GenericMax("Apple", "Banana", "Pineapple", "Ice", "An");
         String maxString = (String) genericMax.maximumGeneric();
         Assertions.assertEquals(String.valueOf("Pineapple"),maxString);
     }
