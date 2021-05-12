c1309d161f11079c0fb4abe0ed8a3e7f392b61be
ayurn <65171090+ayurn@users.noreply.github.com>
2021-05-13 00:37:56 +0530

    Refactored : using one generic method for all data types

--- src/main/java/GenericMax.java
+++ src/main/java/GenericMax.java
@@ -1,6 +1,19 @@
-public class GenericMax {
+//Refactor-2 Refactor to create Generic Class to take in 3 variables of Generic Type
+public class GenericMax<E extends Comparable> {
 
-    //Refactor-1 Refactor all the 3 to One Generic Method and find the maximum
+    E firstPosition;
+    E secondPosition;
+    E thirdPosition;
+
+    public GenericMax(E firstPosition, E secondPosition, E thirdPosition) {
+        this.firstPosition = firstPosition;
+        this.secondPosition = secondPosition;
+        this.thirdPosition = thirdPosition;
+    }
+
+    public E maximumGeneric(){
+        return genericMaximum(firstPosition,secondPosition,thirdPosition);
+    }
     public <E extends Comparable> E genericMaximum(E firstPosition, E secondPosition, E thirdPosition){
         E maxPosition=firstPosition;
         if(secondPosition.compareTo(maxPosition) > 0)
@@ -8,39 +21,5 @@ public class GenericMax {
         if(thirdPosition.compareTo(maxPosition) > 0)
             maxPosition=thirdPosition;
         return maxPosition;
-    }
-
-    //uc1
-    public Integer MaximumInteger(Integer firstValue, Integer secondValue, Integer thirdValue) {
-        Integer maxInt = firstValue;
-        if (secondValue.compareTo(maxInt) > 0)
-            maxInt = secondValue;
-        if (thirdValue.compareTo(maxInt) > 0)
-            maxInt = thirdValue;
-        return maxInt;
-    }
-    //uc2
-    public Double maxPositionDouble(Double firstDoublePosition, Double secondDoublePosition, Double thirdDoublePosition) {
-        Double maxDouble = firstDoublePosition;
-        if (secondDoublePosition.compareTo(maxDouble) > 0)
-            maxDouble = secondDoublePosition;
-        if (thirdDoublePosition.compareTo(maxDouble) > 0)
-            maxDouble = thirdDoublePosition;
-        return maxDouble;
-    }
-
-    //uc3
-    //String
-    public String maxPositionString(String firstStringPosition, String secondStringPosition, String thirdStringPosition) {
-        String maxString = firstStringPosition;
-        if (secondStringPosition.compareTo(maxString) > 0)
-            maxString = secondStringPosition;
-        if (thirdStringPosition.compareTo(maxString) > 0)
-            maxString = thirdStringPosition;
-        return maxString;
-    }
-
-    public static void main(String[] args) {
-        System.out.println("Welcome to program");
     }
 }
--- src/test/java/GenericMaxTest.java
+++ src/test/java/GenericMaxTest.java
@@ -7,92 +7,92 @@ public class GenericMaxTest {
     // Integer
     @Test
     public void givenThreeIntegers_shouldReturnMaxPosition(){
-        GenericMax genericMax = new GenericMax();
-        Integer maxInteger = genericMax.MaximumInteger(9,5,2);
-        Assertions.assertEquals(Integer.valueOf(9),maxInteger);
+        GenericMax genericMax = new GenericMax(9,5,2);
+        Integer maxInt = (Integer) genericMax.maximumGeneric();
+        Assertions.assertEquals(Integer.valueOf(9),maxInt);
     }
 
     //Float
     @Test
     public void givenThreeFloats_shouldReturnMaxPosition(){
-        GenericMax genericMax = new GenericMax();
-        Double maxDouble = genericMax.maxPositionDouble(2.5,2.6,2.1);
-        Assertions.assertEquals(Double.valueOf(2.6),maxDouble);
+        GenericMax genericMax = new GenericMax(2.3,2.1,2.2);
+        Double maxFloat = (Double) genericMax.maximumGeneric();
+        Assertions.assertEquals(Double.valueOf(2.3),maxFloat);
     }
 
     //String
     @Test
     public void givenThreeStrings_shouldReturnMaxPosition(){
-        GenericMax genericMax = new GenericMax();
-        String maxString = genericMax.maxPositionString("Pineapple", "Apple", "Banana");
+        GenericMax genericMax = new GenericMax("Pineapple", "Apple", "Banana");
+        String maxString = (String) genericMax.maximumGeneric();
         Assertions.assertEquals(String.valueOf("Pineapple"),maxString);
     }
-
+    //refactor2
     @Test
     public void FirstNumMaxTest() {
-        GenericMax genericMax = new GenericMax();
-        Integer maxInteger = genericMax.MaximumInteger(9,5,2);
-        Assertions.assertEquals(Integer.valueOf(9),maxInteger);
+        GenericMax genericMax = new GenericMax(9,5,2);
+        Integer maxInt = (Integer) genericMax.maximumGeneric();
+        Assertions.assertEquals(Integer.valueOf(9),maxInt);
     }
 
     @Test
     public void SecondNumMaxTest() {
-        GenericMax genericMax = new GenericMax();
-        Integer maxInteger = genericMax.MaximumInteger(9,15,8);
-        Assertions.assertEquals(Integer.valueOf(15),maxInteger);
+        GenericMax genericMax = new GenericMax(5,9,2);
+        Integer maxInt = (Integer) genericMax.maximumGeneric();
+        Assertions.assertEquals(Integer.valueOf(9),maxInt);
     }
 
     @Test
     public void ThirdNumMaxTest() {
-        GenericMax genericMax = new GenericMax();
-        Integer maxInteger = genericMax.MaximumInteger(9,15,21);
-        Assertions.assertEquals(Integer.valueOf(21),maxInteger);
+        GenericMax genericMax = new GenericMax(9,5,21);
+        Integer maxInt = (Integer) genericMax.maximumGeneric();
+        Assertions.assertEquals(Integer.valueOf(21),maxInt);
     }
 
     //UC-2 Given 3 Floats find the maximum //TC--2.1 //Floats
     @Test
     public void givenMaxFloatAtFirstPosition(){
-        GenericMax genericMax = new GenericMax();
-        Double maxDouble = genericMax.maxPositionDouble(2.5,2.3,2.1);
-        Assertions.assertEquals(Double.valueOf(2.5),maxDouble);
+        GenericMax genericMax = new GenericMax(2.3,2.1,2.2);
+        Double maxFloat = (Double) genericMax.maximumGeneric();
+        Assertions.assertEquals(Double.valueOf(2.3),maxFloat);
     }
 
     //TC--2.2
     @Test
     public void givenMaxFloatAtSecondPosition(){
-        GenericMax genericMax = new GenericMax();
-        Double maxDouble = genericMax.maxPositionDouble(2.5,2.6,2.1);
-        Assertions.assertEquals(Double.valueOf(2.6),maxDouble);
+        GenericMax genericMax = new GenericMax(2.3,2.34,2.2);
+        Double maxFloat = (Double) genericMax.maximumGeneric();
+        Assertions.assertEquals(Double.valueOf(2.34),maxFloat);
     }
 
     //TC--2.3
     @Test
     public void givenMaxFloatAtThirdPosition(){
-        GenericMax genericMax = new GenericMax();
-        Double maxDouble = genericMax.maxPositionDouble(2.5,2.3,2.7);
-        Assertions.assertEquals(Double.valueOf(2.7),maxDouble);
+        GenericMax genericMax = new GenericMax(2.3,2.1,2.23);
+        Double maxFloat = (Double) genericMax.maximumGeneric();
+        Assertions.assertEquals(Double.valueOf(2.23),maxFloat);
     }
-
+    //TC--3.1
     @Test
     public void givenMaxStringAtFirstPosition_shouldReturnSame(){
-        GenericMax genericMax = new GenericMax();
-        String maxString = genericMax.maxPositionString("Pineapple", "Apple", "Banana");
+        GenericMax genericMax = new GenericMax("Pineapple", "Apple", "Banana");
+        String maxString = (String) genericMax.maximumGeneric();
         Assertions.assertEquals(String.valueOf("Pineapple"),maxString);
     }
 
     //TC--3.2
     @Test
     public void givenMaxStringAtSecondPosition_shouldReturnSame(){
-        GenericMax genericMax = new GenericMax();
-        String maxString = genericMax.maxPositionString("Apple", "Pineapple", "Banana");
+        GenericMax genericMax = new GenericMax("Apple", "Pineapple", "Banana");
+        String maxString = (String) genericMax.maximumGeneric();
         Assertions.assertEquals(String.valueOf("Pineapple"),maxString);
     }
 
     //TC--3.3
     @Test
     public void givenMaxStringAtThirdPosition_shouldReturnSame(){
-        GenericMax genericMax = new GenericMax();
-        String maxString = genericMax.maxPositionString("Apple", "Banana", "Pineapple");
+        GenericMax genericMax = new GenericMax("Apple", "Banana", "Pineapple");
+        String maxString = (String) genericMax.maximumGeneric();
         Assertions.assertEquals(String.valueOf("Pineapple"),maxString);
     }
 
