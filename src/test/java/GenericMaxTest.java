b29d9603c43e7ac8863c309c6142987a0c55ffa2
ayurn <65171090+ayurn@users.noreply.github.com>
2021-05-12 23:41:21 +0530

    Added : code for second max int

--- src/test/java/GenericMaxTest.java
+++ src/test/java/GenericMaxTest.java
@@ -3,12 +3,17 @@ import org.junit.jupiter.api.Test;
 
 public class GenericMaxTest {
 
-
-
     @Test
     public void FirstNumMaxTest() {
         GenericMax genericMax = new GenericMax();
         Integer maxInteger = genericMax.MaximumIntiger(9,5,2);
         Assertions.assertEquals(Integer.valueOf(9),maxInteger);
+    }
+
+    @Test
+    public void SecondNumMaxTest() {
+        GenericMax genericMax = new GenericMax();
+        Integer maxInteger = genericMax.MaximumIntiger(9,15,8);
+        Assertions.assertEquals(Integer.valueOf(15),maxInteger);
     }
 }
