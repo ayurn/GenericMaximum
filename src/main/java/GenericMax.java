8a1b99ec341e5da768454115843665fc556e4b89
ayurn <65171090+ayurn@users.noreply.github.com>
2021-05-13 00:04:35 +0530

    Added : code for maximum string

--- src/main/java/GenericMax.java
+++ src/main/java/GenericMax.java
@@ -1,7 +1,7 @@
 public class GenericMax {
 
     //uc1
-    public Integer MaximumIntiger(Integer firstValue, Integer secondValue, Integer thirdValue) {
+    public Integer MaximumInteger(Integer firstValue, Integer secondValue, Integer thirdValue) {
         Integer maxInt = firstValue;
         if (secondValue.compareTo(maxInt) > 0)
             maxInt = secondValue;
@@ -9,7 +9,7 @@ public class GenericMax {
             maxInt = thirdValue;
         return maxInt;
     }
-
+    //uc2
     public Double maxPositionDouble(Double firstDoublePosition, Double secondDoublePosition, Double thirdDoublePosition) {
         Double maxDouble = firstDoublePosition;
         if (secondDoublePosition.compareTo(maxDouble) > 0)
@@ -17,6 +17,17 @@ public class GenericMax {
         if (thirdDoublePosition.compareTo(maxDouble) > 0)
             maxDouble = thirdDoublePosition;
         return maxDouble;
+    }
+
+    //uc3
+    //String
+    public String maxPositionString(String firstStringPosition, String secondStringPosition, String thirdStringPosition) {
+        String maxString = firstStringPosition;
+        if (secondStringPosition.compareTo(maxString) > 0)
+            maxString = secondStringPosition;
+        if (thirdStringPosition.compareTo(maxString) > 0)
+            maxString = thirdStringPosition;
+        return maxString;
     }
 
     public static void main(String[] args) {
--- src/test/java/GenericMaxTest.java
+++ src/test/java/GenericMaxTest.java
@@ -6,21 +6,21 @@ public class GenericMaxTest {
     @Test
     public void FirstNumMaxTest() {
         GenericMax genericMax = new GenericMax();
-        Integer maxInteger = genericMax.MaximumIntiger(9,5,2);
+        Integer maxInteger = genericMax.MaximumInteger(9,5,2);
         Assertions.assertEquals(Integer.valueOf(9),maxInteger);
     }
 
     @Test
     public void SecondNumMaxTest() {
         GenericMax genericMax = new GenericMax();
-        Integer maxInteger = genericMax.MaximumIntiger(9,15,8);
+        Integer maxInteger = genericMax.MaximumInteger(9,15,8);
         Assertions.assertEquals(Integer.valueOf(15),maxInteger);
     }
 
     @Test
     public void ThirdNumMaxTest() {
         GenericMax genericMax = new GenericMax();
-        Integer maxInteger = genericMax.MaximumIntiger(9,15,21);
+        Integer maxInteger = genericMax.MaximumInteger(9,15,21);
         Assertions.assertEquals(Integer.valueOf(21),maxInteger);
     }
 
