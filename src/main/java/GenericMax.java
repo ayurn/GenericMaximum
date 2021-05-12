8c6a60c44b3c5f9c6c21ec00ab4ba31f10709e0c
ayurn <65171090+ayurn@users.noreply.github.com>
2021-05-12 23:22:57 +0530

    Added : code for finding max int

--- src/main/java/GenericMax.java
+++ src/main/java/GenericMax.java
@@ -1,5 +1,15 @@
 public class GenericMax {
 
+    //uc1
+    public Integer MaximumIntiger(Integer firstValue, Integer secondValue, Integer thirdValue) {
+        Integer maxInt = firstValue;
+        if (secondValue.compareTo(maxInt) > 0)
+            maxInt = secondValue;
+        if (thirdValue.compareTo(maxInt) > 0)
+            maxInt = thirdValue;
+        return maxInt;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to program");
     }
